30c726c4eb9a1a8bdc6e6eef4a7e2187be7dd341
Andre <andreluizrodrigues14@gmail.com>
2021-10-17 16:38:20 -0400

    Subindo Arquivos
    Criado a janela

---
+++ OpenGL TK/Game.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+
+namespace OpenGL_TK
+{
+    class Game
+    {
+        GameWindow window;
+        public Game(GameWindow window)
+        {
+            this.window = window;
+            Start();
+        }
+        void Start()
+        {
+            window.Load += loaded;
+            window.Resize += resize;
+            window.RenderFrame += renderF;
+            window.Run(1.0 / 60.0);
+        }
+        void resize(object ob, EventArgs e)
+        {
+            GL.Viewport(0, 0, window.Width, window.Height);
+            GL.MatrixMode(MatrixMode.Projection);
+            GL.LoadIdentity();
+            GL.Ortho(0.0f, 50.0f, 0.0f, 50.0f, -1.0f, 1.0f);
+            GL.MatrixMode(MatrixMode.Modelview);
+        }
+        void renderF(object o, EventArgs e)
+        {
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+            window.SwapBuffers();
+        }
+
+        void loaded(object o, EventArgs e)
+        {
+            GL.ClearColor(0.0f,0.0f,0.0f,0.0f);
+        }
+    }
+}
---
+++ OpenGL TK/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using OpenTK;
+namespace OpenGL_TK
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GameWindow window = new GameWindow(300, 300);
+            Game gm = new Game(window);
+        }
+
+    }
+}
