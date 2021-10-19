90f250ff69576a09c5588a666e9136757b04be58
Andre <andreluizrodrigues14@gmail.com>
2021-10-18 23:43:49 -0400

    Restaurado branch main

--- OpenGL TK/Game.cs
+++ OpenGL TK/Game.cs
@@ -7,6 +7,7 @@ namespace OpenGL_TK
     class Game
     {
         double theta = 0.0;
+        double eixoX = 5, eixoY = 5, eixoZ = 5;
         GameWindow window;
         public Game(GameWindow window)
         {
@@ -18,7 +19,7 @@ namespace OpenGL_TK
             window.Load += loaded;
             window.Resize += resize;
             window.RenderFrame += renderF;
-            window.Run(1.0 / 60.0);
+            window.Run(1.0, 60.0);
         }
         void resize(object ob, EventArgs e)
         {
@@ -26,21 +27,22 @@ namespace OpenGL_TK
             GL.MatrixMode(MatrixMode.Projection);
             GL.LoadIdentity();
 
-            // Plano Cartesiano / tamanho dos quadrantes
-            GL.Ortho(-50.0f, 50.0f, -50.0f, 50.0f, -1.0f, 1.0f);
+            Matrix4 matrix = Matrix4.Perspective(45.0f, window.Width / window.Height, 1.0f, 100.0f);
+            GL.LoadMatrix(ref matrix);
             GL.MatrixMode(MatrixMode.Modelview);
         }
         void renderF(object o, EventArgs e)
         {
             GL.LoadIdentity();
-            GL.Clear(ClearBufferMask.ColorBufferBit);
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
 
+            // Movido para traz para que possa ser renderizado
+            GL.Translate(0.0, 0.0, -45.0);
+            GL.Rotate(theta, 1.0, 0.0, 0.0);
             GL.Rotate(theta, 0.0, 0.0, 1.0);
+
             GL.Begin(BeginMode.Quads);
-            DrawCube();
-            
-            //GL.Begin(BeginMode.Triangles);
-            //DrawTriangle();
+            CoordenadasCubo();
             GL.End();
             window.SwapBuffers();
 
@@ -48,7 +50,46 @@ namespace OpenGL_TK
             if (theta > 360)
                 theta -= 360;
         }
-        void DrawCube()
+        void CoordenadasCubo()
+        {
+            GL.Color3(1.0, 1.0, 0.0);
+            GL.Vertex3(-eixoX, eixoY, eixoZ);
+            GL.Vertex3(-eixoX, eixoY, -eixoZ);
+            GL.Vertex3(-eixoX, -eixoY, -eixoZ);
+            GL.Vertex3(-eixoX, -eixoY, eixoZ);
+
+            GL.Color3(1.0, 0.0, 1.0);
+            GL.Vertex3(eixoX, eixoY, eixoZ);
+            GL.Vertex3(eixoX, eixoY, -eixoZ);
+            GL.Vertex3(eixoX, -eixoY, -eixoZ);
+            GL.Vertex3(eixoX, -eixoY, eixoZ);
+
+            GL.Color3(0.0, 1.0, 1.0);
+            GL.Vertex3(eixoX, -eixoY, eixoZ);
+            GL.Vertex3(eixoX, -eixoY, -eixoZ);
+            GL.Vertex3(-eixoX, -eixoY, -eixoZ);
+            GL.Vertex3(-eixoX, -eixoY, eixoZ);
+
+            GL.Color3(1.0, 0.0, 0.0);
+            GL.Vertex3(eixoX, eixoY, eixoZ);
+            GL.Vertex3(eixoX, eixoY, -eixoZ);
+            GL.Vertex3(-eixoX, eixoY, -eixoZ);
+            GL.Vertex3(-eixoX, eixoY, eixoZ);
+
+            GL.Color3(0.0, 1.0, 0.0);
+            GL.Vertex3(eixoX, eixoY, -eixoZ);
+            GL.Vertex3(eixoX, -eixoY, -eixoZ);
+            GL.Vertex3(-eixoX, -eixoY, -eixoZ);
+            GL.Vertex3(-eixoX, eixoY, -eixoZ);
+
+            GL.Color3(0.0, 0.0, 1.0);
+            GL.Vertex3(eixoX, eixoY, eixoZ);
+            GL.Vertex3(eixoX, -eixoY, eixoZ);
+            GL.Vertex3(-eixoX, -eixoY, eixoZ);
+            GL.Vertex3(-eixoX, eixoY, eixoZ);
+
+        }
+        void DrawQuad()
         {
             GL.Color3(1.0, 0.0, 0.0);
             GL.Vertex2(20.0, 20.0);
@@ -73,7 +114,11 @@ namespace OpenGL_TK
         }
         void loaded(object o, EventArgs e)
         {
-            GL.ClearColor(0.0f,0.0f,0.0f,0.0f);
+            GL.ClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+
+            // Habilita profundidade,
+            // caso contrário os objetos desenhados ficam sobrepostos
+            GL.Enable(EnableCap.DepthTest);
         }
     }
 }
